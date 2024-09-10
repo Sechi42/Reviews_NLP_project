9fee9226af57cc6bff5a38c44e4d7e498126d439
Sergio Anaya <choptop12@gmail.com>
2024-09-10 12:54:01 -0600

    Ajuste del pipeline y nueva funcion de prediccion para la app

--- Monitoring-Prometheus-Grafana/src/prediction_model/pipeline.py
+++ Monitoring-Prometheus-Grafana/src/prediction_model/pipeline.py
@@ -7,6 +7,7 @@ import numpy as np
 BERT_pipeline = Pipeline(
     [
         ('ReviewNormalization', pp.ReviewNormImputer(variable_to_add=config.FEATURES_TO_ADD, variable_to_modify=config.FEATURE_TO_MODIFY)),
+        ("DropColumns", pp.DropColumns(variables_to_drop=config.FEATURE_TO_MODIFY)),
         ('BERTTensorTransform', pp.BERTTextEmbeddingsTransformer(force_device='cuda', column_name=config.FEATURES)),
         ('LogisticClassifier', LogisticRegression(random_state=12345)),
     ]
--- Monitoring-Prometheus-Grafana/src/prediction_model/predict.py
+++ Monitoring-Prometheus-Grafana/src/prediction_model/predict.py
@@ -7,32 +7,20 @@ import torch
 
 BERT_class_pipeline = load_pipeline(config.MODEL_NAME)
 
-'''def generate_predictions(data_input):
+def generate_predictions_1(data_input):
     data = pd.DataFrame(data_input)
-    pred = BERT_class_pipeline(data[config.FEATURES])
+    pred = BERT_class_pipeline(data)
     result = {'Predictions':pred.tolist()}
-    return result'''
+    return result
     
-def generate_predictions(data):
-    # data es la reseña que proviene del API
-    # Aquí asumimos que data es una lista o diccionario de reseñas
-    
-    # Si necesitas convertir los datos a un formato específico, puedes hacerlo aquí
-    df = pd.DataFrame(data)  # Si es una lista de diccionarios, esto funcionará
-    
-    # Generar predicción con el pipeline de BERT (simulado aquí con una condición)
-    pred = BERT_class_pipeline.predict(df)
-    
-    # Convertir las predicciones en 'P' o 'N'
+def generate_predictions():
+    test_data = load_dataset(config.TEST_DATA)
+    pred = BERT_class_pipeline.predict(test_data)
     output = np.where(pred == 1, 'P', 'N')
     print(output)
-    return {"prediction": output}
-
+    return output
 
 
 if __name__=='__main__':
-    # Ejemplo de datos para probar la función
-    example_data = [{"review": "I don't like the movie"}]
-    predictions = generate_predictions(data=example_data)
-    print(predictions)
-    
+    predictions = generate_predictions()
+    pd.DataFrame(predictions, columns=["Predictions"]).to_csv("predictions.csv", index=False)
